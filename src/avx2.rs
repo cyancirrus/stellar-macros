24c4fd34722742ed933e20cc1490834c67c3bc30
autumn <cyancirrus@gmail.com>
2026-05-06 18:15:09 -0500

    thigns

--- src/avx2.rs
+++ src/avx2.rs
@@ -96,8 +96,59 @@ pub fn pack_simd_line_unalligned(
     let stride = unroll_factor * AVX2_SIMD_WIDTH;
     quote ! {
         // x & 7 == x % 8;
-        for o in (0..#BLOCK_WIDTH).step_by(#stride) {
+        for o in (0..#BLOCK_WIDTH ).step_by(#stride) {
             #(#unroll)*
+        }
+    }.into()
+}
+pub fn pack_simd_line(
+    input: proc_macro::TokenStream
+) -> proc_macro::TokenStream {
+    // c :: clean, d :: dirty
+    let unroll_factor = 4;
+    let args = parse_macro_input!(input as PackUSimdArgs);
+    let PackUSimdArgs { bptr, dptr, source } = args;
+    let mut c_unroll = Vec::new();
+    let mut d_unroll = Vec::new();
+    
+    // AVX2_SIMD_WIDTH = 8 = 2^3;
+    let stride = unroll_factor * AVX2_SIMD_WIDTH;
+    for o in (0..unroll_factor * AVX2_SIMD_WIDTH).step_by(AVX2_SIMD_WIDTH) {
+        c_unroll.push(
+            quote! {
+                    _mm256_storeu_ps(
+                        #bptr.add(i + #o),
+                        _mm256_loadu_ps(#dptr.add(i + #o))
+                    );
+            }
+        );
+    }
+    for o in (0..unroll_factor * AVX2_SIMD_WIDTH).step_by(AVX2_SIMD_WIDTH) {
+        d_unroll.push(
+            quote! {
+                _mm256_storeu_ps(
+                    #bptr.add(offset + #o),
+                    _mm256_maskload_ps(
+                        #dptr.add(offset + #o),
+                        _mm256_loadu_si256(MASK[#source.saturating_sub(offset + #o).min(8)].as_ptr() as *const __m256i)
+                    )
+                );
+            }
+        );
+    }
+    let z_roll = quote! {
+            _mm256_storeu_ps( #bptr.add(o), zeros);
+    };
+    // offset == number of clean iterations
+    quote ! {
+        let zeros = _mm256_setzero_ps();
+        let offset = #source & !(#AVX2_SIMD_WIDTH - 1);
+        for i in (0..offset).step_by(#stride) {
+            #(#c_unroll)*
+        }
+        #(#d_unroll)*
+        for o in (offset + #AVX2_SIMD_WIDTH..#BLOCK_WIDTH).step_by(#AVX2_SIMD_WIDTH) {
+            #z_roll
         }
     }.into()
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,11 @@ use proc_macro::TokenStream;
 mod avx2;
 
 #[proc_macro]
+pub fn avx2_pack_simd_line(input: TokenStream) -> TokenStream {
+    avx2::pack_simd_line(input)
+}
+
+#[proc_macro]
 pub fn avx2_pack_simd_line_alligned(input: TokenStream) -> TokenStream {
     avx2::pack_simd_line_alligned(input)
 }
