21f6f2ce158d1d14b3a700db24e6dabdbd8f1507
autumn <cyancirrus@gmail.com>
2026-06-22 14:13:29 -0500

    saving and checkout out main

--- src/lib.rs
+++ src/lib.rs
@@ -5,18 +5,18 @@ mod instructs;
 const M: usize = 8;
 const B: usize = 4;
 
-// #[proc_macro]
-// pub fn avx2_pack_simd_line(input: TokenStream) -> TokenStream {
-//     avx2::pack::pack_simd_line(input)
-// }
-// #[proc_macro]
-// pub fn avx2_pack_simd_line_alligned(input: TokenStream) -> TokenStream {
-//     avx2::pack::pack_simd_line_alligned(input)
-// }
-// #[proc_macro]
-// pub fn avx2_pack_simd_line_unalligned(input: TokenStream) -> TokenStream {
-//     avx2::pack::pack_simd_line_unalligned(input)
-// }
+#[proc_macro]
+pub fn avx2_pack_simd_line(input: TokenStream) -> TokenStream {
+    avx2::pack::pack_simd_line(input)
+}
+#[proc_macro]
+pub fn avx2_pack_simd_line_alligned(input: TokenStream) -> TokenStream {
+    avx2::pack::pack_simd_line_alligned(input)
+}
+#[proc_macro]
+pub fn avx2_pack_simd_line_unalligned(input: TokenStream) -> TokenStream {
+    avx2::pack::pack_simd_line_unalligned(input)
+}
 #[proc_macro]
 pub fn kernel_lmult_lower_tri(input: TokenStream) -> TokenStream {
     avx2::kernel::matmul::lmult_lower_tri(input, M, B)
@@ -24,6 +24,14 @@ pub fn kernel_lmult_lower_tri(input: TokenStream) -> TokenStream {
 #[proc_macro]
 pub fn kernel_lmult_upper_tri(input: TokenStream) -> TokenStream {
     avx2::kernel::matmul::lmult_upper_tri(input, M, B)
+}
+#[proc_macro]
+pub fn kernel_mult_alligned(input: TokenStream) -> TokenStream {
+    avx2::kernel::matmul::mult_alligned(input, M, B)
+}
+#[proc_macro]
+pub fn kernel_mult_unalligned(input: TokenStream) -> TokenStream {
+    avx2::kernel::matmul::mult_unalligned(input, M, B)
 }
 // #[proc_macro]
 // pub fn kernel_tmult_alligned(input: TokenStream) -> TokenStream {
